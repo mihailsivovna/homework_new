a3279065d4489d0f522e4cb30a3da25f3619e3ac
mihailsivovna <mihailsivovna@gmail,com>
2018-10-03 18:46:15 +0300

    home

--- Test-console/Program.cs
+++ Test-console/Program.cs
@@ -4,15 +4,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test_console
+namespace Automation.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.WriteLine("Hello world");
-            Console.ReadLine();
+
+            {
+                string username = "name";
+                string password = "password";
+                bool result;
+
+
+
+                var k = 1;
+
+                do
+                {
+
+
+                    Console.Write("Введите логин: ");
+                    var name = Console.ReadLine();
+                    Console.Write("Введите пароль: ");
+                    var pass = Console.ReadLine();
+
+
+
+                    if (result = true)
+                    {
+                        if (username == name && password == pass)
+
+
+                        {
+
+                            Console.WriteLine("Успешный вход");
+                            break;
+                        }
+                    }
+
+                    else
+
+                        result = false;
+
+                    {
+                        Console.WriteLine("Неправильный логин или пароль");
+
+
+                    }
+
+                    Console.WriteLine();
+                    k++;
+
+
+
+                } while (k <= 5);
+
+
+
+                if (k > 5)
+
+                {
+                    Console.WriteLine(" Аккаунт заблокирован. Повторите попытку позже ");
+
+                }
+
+
+
+                Console.ReadLine();
+
+
+
+            }
+
         }
     }
 }
+
+
